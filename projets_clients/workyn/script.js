416a495a43501d560199c27d7a907e4942344f90
paulstilhart <paul.stilhart@gmail.com>
2023-02-20 10:21:51 +0000

    workyn

--- projets_clients/workyn/script.js
+++ projets_clients/workyn/script.js
@@ -24,16 +24,16 @@ const footer = document.querySelector("footer");
 
 
 //Au click sur le bouton, on vient déclencher la fonction callback toggleNav
-burgerButton.addEventListener("click",toggleNav);
+burgerButton.addEventListener("click", toggleNav);
 
 //Au click à l'extérieur de du menu une fois ouvert, on vient déclencher la fonction callback toggleNavIfOpen
-body.addEventListener("click",toggleNavIfOpen);
-footer.addEventListener("click",toggleNavIfOpen);
+body.addEventListener("click", toggleNavIfOpen);
+footer.addEventListener("click", toggleNavIfOpen);
 
 
 //on définit la fonction toggleNav
 //toggle permet d'ajouter la classe si pas présente, l'enlever si déjà présente
-function toggleNav(){
+function toggleNav() {
     burgerButton.classList.toggle("active");
     navigation.classList.toggle("active");
     header.classList.toggle("active");
@@ -41,10 +41,37 @@ function toggleNav(){
 
 
 //Permet de fermer le menu en cliquant à l'extérieur
-function toggleNavIfOpen(){
+function toggleNavIfOpen() {
     if (header.classList.contains("active")) {
         burgerButton.classList.toggle("active");
         navigation.classList.toggle("active");
         header.classList.toggle("active");
     }
 };
+
+
+/*================================ GESTION DU MENU SUIVANT LA HAUTEUR =======================================*/
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+    /* console.dir(document.documentElement); //permet de voir toutes les propriétés dispos
+        const { scrollTop, scrollHeight, clientHeight }
+        = document.documentElement;
+    console.log(scrollTop, scrollHeight, clientHeight);
+    dans le cas ou on aurait besoin de toutes ces propriétées, ici en l'occurence juste celle vers le haut
+    */
+
+    const { scrollTop } = document.documentElement;
+
+    if ((scrollTop > lastScrollTop) & (scrollTop > 500)) {
+        header.classList.add("dnone");
+        console.log('Scroll vers le bas');
+    };
+
+    if ((scrollTop < lastScrollTop)) {
+        header.classList.remove("dnone");
+    };
+
+    lastScrollTop = scrollTop;
+
+});
