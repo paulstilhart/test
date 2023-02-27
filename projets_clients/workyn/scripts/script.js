77a8bd4f8aa3a0753db5b9f779de1ffc4ed8527b
paulstilhart <paul.stilhart@gmail.com>
2023-02-27 18:09:22 +0000

    fade in

--- projets_clients/workyn/scripts/script.js
+++ projets_clients/workyn/scripts/script.js
@@ -18,15 +18,15 @@ function Mail() {
 const burgerButton = document.querySelector(".nav_toggler");//On vient récupérer le bouton du burger menu
 const navigation = document.querySelector("nav");//egalement l'élément nav
 const header = document.querySelector("header");//egalement l'élément nav
-const body = document.querySelector("main");
+const main = document.querySelector("main");
 const footer = document.querySelector("footer");
+const sections = document.querySelectorAll("section");
+const text = document.querySelectorAll("h1,h2,h3");
 
 const sectionApproach = document.querySelector(".section_approach");
 const cardsContainer = document.querySelector(".section_approach_cards_container");
 const previous = document.getElementById("section_approach_cards_button_previous");
 const next = document.getElementById("section_approach_cards_button_next");
-const nombrecartes = cardsContainer.children.length;
-const firstcard = cardsContainer.firstElementChild;
 const cards = document.querySelectorAll('.section_approach_card');
 
 
@@ -35,7 +35,7 @@ const cards = document.querySelectorAll('.section_approach_card');
 burgerButton.addEventListener("click", toggleNav);
 
 //Au click à l'extérieur de du menu une fois ouvert, on vient déclencher la fonction callback toggleNavIfOpen
-body.addEventListener("click", toggleNavIfOpen);
+main.addEventListener("click", toggleNavIfOpen);
 footer.addEventListener("click", toggleNavIfOpen);
 
 
@@ -60,6 +60,25 @@ function toggleNavIfOpen() {
 
 /*================================ GESTION DU MENU SUIVANT LA HAUTEUR =======================================*/
 var lastScrollTop = 0;
+let options = {
+    //root: null,
+    //rootMargin: '0',
+    //threshold: 0.25,
+}
+const observer = new IntersectionObserver(handleIntersect, options);
+
+function handleIntersect(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fadein");
+        };
+        if (!(entry.isIntersecting)) {
+            entry.target.classList.remove("fadein");
+        };
+    })
+};
+
+//pour mettre tous les titres à opacity 0
 
 window.addEventListener('scroll', () => {
     /* console.dir(document.documentElement); //permet de voir toutes les propriétés dispos
@@ -68,6 +87,9 @@ window.addEventListener('scroll', () => {
     console.log(scrollTop, scrollHeight, clientHeight);
     dans le cas ou on aurait besoin de toutes ces propriétées, ici en l'occurence juste celle vers le haut
     */
+    text.forEach(item => {
+        observer.observe(item);
+    })
     const { scrollTop } = document.documentElement;
     if (scrollTop > 500) {
         header.classList.add("header_big");
@@ -85,13 +107,15 @@ window.addEventListener('scroll', () => {
 
     lastScrollTop = scrollTop;
 
-    if (window.innerWidth > 2600) {
-        next.classList.remove("active");
-        return;//on sort de la boucle
-    };
-    if ((window.innerWidth) < 2600 & !(previous.classList.contains("active"))) {
-        next.classList.add("active");
-        return;//on sort de la boucle
+    if(next){
+        if(window.innerWidth > 2600) {
+            next.classList.remove("active");
+            return;//on sort de la boucle
+        };
+        if ((window.innerWidth) < 2600 & !(previous.classList.contains("active"))) {
+            next.classList.add("active");
+            return;//on sort de la boucle
+        }
     }
 });
 
@@ -150,7 +174,7 @@ function enable() {
 //Click bouton next
 next.addEventListener("click", event => {
     //on récupere le decalage de la premiere carte en unité positive ou négativ
-    let decalage = Math.round(((firstcard.getBoundingClientRect().x) - (cardsContainer.offsetLeft)) / 413);
+    let decalage = Math.round(((cardsContainer.firstElementChild.getBoundingClientRect().x) - (cardsContainer.offsetLeft)) / 413);
     let lastCardInner = (window.innerWidth - ((cardsContainer.lastElementChild).getBoundingClientRect().x));
 
 
@@ -158,7 +182,7 @@ next.addEventListener("click", event => {
         return;//on sort de la boucle
     };
 
-    if (decalage > (-nombrecartes + 1) & lastCardInner > 0) {
+    if (decalage > (-cardsContainer.children.length + 1) & lastCardInner > 0) {
         next.classList.remove("active");
         previous.classList.add("active");
         translateXvalue = 'translateX('.concat(((decalage * 413) - 413), 'px)');
@@ -169,7 +193,7 @@ next.addEventListener("click", event => {
         return;//on sort de la boucle
     };
 
-    if (decalage > (-nombrecartes + 1)) {
+    if (decalage > (-cardsContainer.children.length + 1)) {
         previous.classList.add("active");
         translateXvalue = 'translateX('.concat(((decalage * 413) - 413), 'px)');
 
@@ -191,7 +215,7 @@ next.addEventListener("click", event => {
 previous.addEventListener("click", event => {
 
     //on récupere le decalage de la premiere carte en unité positive ou négative
-    let decalage = Math.round(((firstcard.getBoundingClientRect().x) - (cardsContainer.offsetLeft)) / 413);
+    let decalage = Math.round(((cardsContainer.firstElementChild.getBoundingClientRect().x) - (cardsContainer.offsetLeft)) / 413);
 
     if (decalage > (-0.5)) {
         return;//on sort de la boucle
