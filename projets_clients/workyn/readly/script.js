1627862cb3bf3fbcc026f55cfd68be0d96ce42e7
paulstilhart <paul.stilhart@gmail.com>
2023-05-04 18:04:35 +0100

    readly

--- projets_clients/workyn/readly/script.js
+++ projets_clients/workyn/readly/script.js
@@ -1,3 +1,12 @@
+/*================================ SESSION STORAGE CLEAN =======================================*/
+const trigger_clean_storage_js = document.querySelector('.trigger_clean_storage_js');//tous les triggers suivants
+
+trigger_clean_storage_js.addEventListener("click", function (event) {
+    event.stopPropagation();
+    sessionStorage.clear();
+});
+
+
 /*================================ TRIGGER JS =======================================*/
 const trigger_next_js = document.querySelectorAll('.trigger_next_js');//tous les triggers suivants
 
@@ -14,5 +23,39 @@ const inputs = document.querySelectorAll('input');//tous les labels
 
 inputs.forEach(input => input.addEventListener("click", function (event) {
     event.stopPropagation();
-    let choice = input.id;
+    const question = input.name;
+    const choice = input.value;
+    sessionStorage.setItem(question, choice);
+}));
+
+
+function findMostAnswered() {
+    const answerCounts = {};
+    for (let i = 0; i < sessionStorage.length; i++) {
+        const key = sessionStorage.key(i);
+        const answer = sessionStorage.getItem(key);
+        if (!answerCounts[answer]) {
+            answerCounts[answer] = 1;
+        } else {
+            answerCounts[answer]++;
+        }
+    }
+    let maxAnswerCount = 0;
+    let mostAnswered = "";
+    for (const answer in answerCounts) {
+        if (answerCounts[answer] > maxAnswerCount) {
+            maxAnswerCount = answerCounts[answer];
+            mostAnswered = answer;
+        }
+    }
+    return mostAnswered;
+};
+
+const trigger_profile_js = document.querySelectorAll('.trigger_profile_js');//tous les labels
+trigger_profile_js.forEach(input => input.addEventListener("click", function (event) {
+    event.stopPropagation();
+    let mostAnswered = findMostAnswered();
+    sessionStorage.setItem('profile', mostAnswered);
+    console.log(mostAnswered);
+    return mostAnswered;
 }));
