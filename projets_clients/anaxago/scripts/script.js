5128fb557afabb27d2458104df8402f52df73689
paulstilhart <paul.stilhart@gmail.com>
2023-06-14 19:21:39 +0100

    anaxago

--- projets_clients/anaxago/scripts/script.js
+++ projets_clients/anaxago/scripts/script.js
@@ -1,23 +1,12 @@
 //*==================== SCRIPT SECTION MANIFESTE ====================*//
 const recrutement_section_manifeste = document.querySelector('.recrutement_section_manifeste');//la section
 const slider = recrutement_section_manifeste.querySelector('.slider');//le container des cartes = slider
+const cards = recrutement_section_manifeste.querySelectorAll('.slider>div');
 const nombreCartes = slider.childElementCount;
 const button_box = recrutement_section_manifeste.querySelector('.button_box');//la button box
 const button_previous = button_box.querySelectorAll('.button_box>button')[0];
 const button_next = button_box.querySelectorAll('.button_box>button')[1];
 
-//Pour remettre le slider à 0 si on resize
-let resizeTimeout; // Variable pour stocker le délai de temporisation
-window.addEventListener('resize', function () {
-    clearTimeout(resizeTimeout); // Réinitialise le délai de temporisation à chaque redimensionnement
-    resizeTimeout = setTimeout(function () {
-        if (window.innerWidth < window.outerWidth - 10) { // Vérifie si le redimensionnement est horizontal avec une marge de 10 pixels
-            // Effectue les actions nécessaires lors du redimensionnement horizontal
-            slider.style.transform = 'translateX(0px)';
-            slider.setAttribute('data-translate', 0);
-        }
-    }, 200); // Délai de temporisation de 200 millisecondes pour limiter les appels fréquents lors du redimensionnement
-});
 
 //Click bouton next
 button_next.addEventListener("click", event => {
@@ -26,25 +15,28 @@ button_next.addEventListener("click", event => {
     let cardWidth = last_card.offsetWidth;//largeur de la carte
     let gap = parseInt((window.getComputedStyle(slider).getPropertyValue('column-gap')).replace("px", ""));//on recupere le gap
 
-
     if (decalage < (-cardWidth)) {
         let currentTranslateX = parseInt(slider.getAttribute('data-translate'));
         let nextTranslateX = (currentTranslateX - (cardWidth + gap));
         translateXvalue = 'translateX('.concat(nextTranslateX, 'px)');
-        slider.style["transform"] = translateXvalue;
+        cards.forEach(card => {
+            card.style["transform"] = translateXvalue;
+        });
         slider.setAttribute('data-translate', nextTranslateX);
         button_previous.classList.add('active');
         return;//on sort de la boucle
-    }
-    else if ((decalage > (-cardWidth - 1)) && (decalage < 0)) {
+
+    } else if ((decalage > (-cardWidth - 1)) && (decalage < 0)) {
         let currentTranslateX = parseInt(slider.getAttribute('data-translate'));
         let nextTranslateX = (currentTranslateX - (cardWidth + gap));
         translateXvalue = 'translateX('.concat(nextTranslateX, 'px)');
-        slider.style["transform"] = translateXvalue;
+        cards.forEach(card => {
+            card.style["transform"] = translateXvalue;
+        });
         slider.setAttribute('data-translate', nextTranslateX);
         button_next.classList.remove('active');
         return;//on sort de la boucle
-    };
+    }
     return;//on sort de la boucle
 });
 
@@ -60,7 +52,9 @@ button_previous.addEventListener("click", event => {
         let currentTranslateX = parseInt(slider.getAttribute('data-translate'));
         let nextTranslateX = (currentTranslateX + (cardWidth + gap));
         translateXvalue = 'translateX('.concat(nextTranslateX, 'px)');
-        slider.style["transform"] = translateXvalue;
+        cards.forEach(card => {
+            card.style["transform"] = translateXvalue;
+        });
         slider.setAttribute('data-translate', nextTranslateX);
         button_next.classList.add('active');
         return;//on sort de la boucle
@@ -69,11 +63,28 @@ button_previous.addEventListener("click", event => {
         let currentTranslateX = parseInt(slider.getAttribute('data-translate'));
         let nextTranslateX = (currentTranslateX + (cardWidth + gap));
         translateXvalue = 'translateX('.concat(nextTranslateX, 'px)');
-        slider.style["transform"] = translateXvalue;
+        cards.forEach(card => {
+            card.style["transform"] = translateXvalue;
+        });
         slider.setAttribute('data-translate', nextTranslateX);
         button_next.classList.add('active');
         button_previous.classList.remove('active');
         return;//on sort de la boucle
     };
     return;//on sort de la boucle
+});
+
+//Pour remettre le slider à 0 si on resize
+let resizeTimeout; // Variable pour stocker le délai de temporisation
+window.addEventListener('resize', function () {
+    clearTimeout(resizeTimeout); // Réinitialise le délai de temporisation à chaque redimensionnement
+    resizeTimeout = setTimeout(function () {
+        if (window.innerWidth < window.outerWidth - 10) { // Vérifie si le redimensionnement est horizontal avec une marge de 10 pixels
+            // Effectue les actions nécessaires lors du redimensionnement horizontal
+            cards.forEach(card => {
+                card.style["transform"] = 'translateX(0px)';
+            });
+            slider.setAttribute('data-translate', 0);
+        }
+    }, 200); // Délai de temporisation de 200 millisecondes pour limiter les appels fréquents lors du redimensionnement
 });
