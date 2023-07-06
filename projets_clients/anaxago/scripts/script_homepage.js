1f744fb94d99adac7d0699c659c98179c1f13709
paulstilhart <paul.stilhart@gmail.com>
2023-07-06 17:16:51 +0100

    anaxago

---
+++ projets_clients/anaxago/scripts/script_homepage.js
@@ -0,0 +1,24 @@
+//*==================== SCRIPT SECTION OPPORTUNITIES ====================*//
+const homepage_section_opportunities = document.querySelector('.homepage_section_opportunities');
+const homepage_section_opportunities_cards = homepage_section_opportunities.querySelectorAll('.main_container>.card');
+
+
+homepage_section_opportunities_cards.forEach(card => card.addEventListener("mouseenter", event => {
+    if (window.innerWidth>= 1200){
+        homepage_section_opportunities_cards.forEach(card => card.classList.remove('active'));
+        card.classList.add('active');
+    }
+    else{
+        return
+    }
+}));
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth>= 1200){
+        homepage_section_opportunities_cards.forEach(card => card.classList.remove('active'));
+        homepage_section_opportunities.querySelector('.main_container>.card').classList.add('active');
+    }
+    else if(window.innerWidth < 1200){
+        homepage_section_opportunities_cards.forEach(card => card.classList.remove('active'));
+    }
+});
