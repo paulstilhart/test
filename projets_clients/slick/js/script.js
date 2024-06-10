9139b94fb1b4f99b1a7945cf513bd2e58e5cb48f
dev-ashil <dev-ashil@ashil.fr>
2024-06-10 23:38:33 +0200

    slick

--- projets_clients/slick/js/script.js
+++ projets_clients/slick/js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Désactiver le glisser-déposer des liens
-    document.addEventListener('dragstart', function(event) {
-        if (event.target.tagName.toLowerCase() === 'a') {
-            event.preventDefault(); // Empêche le glisser-déposer du lien
-        }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Désactiver le glisser-déposer des liens
+  document.addEventListener("dragstart", function (event) {
+    if (event.target.tagName.toLowerCase() === "a") {
+      event.preventDefault(); // Empêche le glisser-déposer du lien
+    }
+  });
 });
